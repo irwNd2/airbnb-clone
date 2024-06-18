f98e2c6aa5e5c19a671917e5e3c2175fcde97332
irwNd2 <irwandduadua@gmail.com>
2024-06-18 23:23:52 +0700

    styles constant

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
 
 const Login = () => {
+  useWarmUpBrowser();
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Login</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 24,
+  },
+});
 
 export default Login;
